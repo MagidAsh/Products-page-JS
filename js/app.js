80d1a6d436e724d8a7aa09e97f09cd26c278692f
Majid Ash <Magid.Ash.Si@gmail.com>
2024-03-10 08:21:38 -0700

    1 - Definition of the event related to the buttons 2 - Change the style of the selected buttons

--- js/app.js
+++ js/app.js
@@ -2,6 +2,16 @@ const searchInput = document.getElementById("search-input");
 const products = document.querySelectorAll(".product-item");
 const buttons = document.querySelectorAll(".filter");
 
+const changeClass = (filter) => {
+  buttons.forEach((button) => {
+    if (button.dataset.filter === filter) {
+      button.classList.add("selected");
+    } else {
+      button.classList.remove("selected");
+    }
+  });
+};
+
 const searchHandler = (event) => {
   const searchValue = event.target.value.toLowerCase().trim();
   //   console.dir(searchValue);
@@ -22,7 +32,19 @@ searchInput.addEventListener("keyup", searchHandler);
 
 const filterHandler = (event) => {
   const filter = event.target.dataset.filter;
-  console.log(filter);
+  changeClass(filter);
+
+  products.forEach((product) => {
+    const category = product.dataset.category;
+
+    if (filter === "all") {
+      product.style.display = "block";
+    } else {
+      category === filter
+        ? (product.style.display = "block")
+        : (product.style.display = "none");
+    }
+  });
 };
 
 buttons.forEach((button) => {
