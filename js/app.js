ef033e8fd252ebef12f81c3426a43a0c6aac2961
Majid Ash <Magid.Ash.Si@gmail.com>
2024-03-10 09:03:42 -0700

    1 - Get the value of the search price 2 - Filtering products based on price

--- js/app.js
+++ js/app.js
@@ -1,6 +1,9 @@
 const searchInput = document.getElementById("search-input");
 const products = document.querySelectorAll(".product-item");
 const buttons = document.querySelectorAll(".filter");
+const priceButton = document
+  .getElementById("search-price")
+  .querySelector("button");
 
 const changeClass = (filter) => {
   buttons.forEach((button) => {
@@ -28,8 +31,6 @@ const searchHandler = (event) => {
   });
 };
 
-searchInput.addEventListener("keyup", searchHandler);
-
 const filterHandler = (event) => {
   const filter = event.target.dataset.filter;
   changeClass(filter);
@@ -47,6 +48,27 @@ const filterHandler = (event) => {
   });
 };
 
+const searchPriceHandler = (event) => {
+  const searchPrice = +event.target.parentElement.children[0].value; // With the Plus operator we turn the String into Namber
+
+  products.forEach((product) => {
+    const productPrice = product.children[2].innerText;
+    const price = +productPrice.split(" ")[1]; // With the Plus operator we turn the String into Namber
+    console.log(price);
+
+    if (!searchPrice) {
+      product.style.display = "block";
+    } else {
+      searchPrice === price
+        ? (product.style.display = "block")
+        : (product.style.display = "none");
+    }
+  });
+};
+
 buttons.forEach((button) => {
   button.addEventListener("click", filterHandler);
 });
+
+searchInput.addEventListener("keyup", searchHandler);
+priceButton.addEventListener("click", searchPriceHandler);
